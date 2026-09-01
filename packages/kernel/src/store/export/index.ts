30ca87fb15c6f88d45038be9b40e9a44fa72a7f0
Yerzhan <yerzhan.st@gmail.com>
2026-09-01 08:20:45 +0500

    fix(kernel): restore legacy planning backups (#850)

--- packages/kernel/src/store/export/index.ts
+++ packages/kernel/src/store/export/index.ts
@@ -1,17 +1,48 @@
-export { buildExport, importExport, EXPORT_WARNING, EXPORT_DOCUMENT_KIND, ExportSchemaMismatchError } from "./export-op.js";
+export {
+  buildExport,
+  importExport,
+  EXPORT_WARNING,
+  EXPORT_DOCUMENT_KIND,
+  ExportSchemaMismatchError,
+} from "./export-op.js";
 export type {
-  BuildExportDeps, BuildExportResult, ImportExportDeps, ImportResult, ManifestVerification,
+  BuildExportDeps,
+  BuildExportResult,
+  ImportExportDeps,
+  ImportResult,
+  ManifestVerification,
 } from "./export-op.js";
 export {
-  encodeContainer, decodeContainer, canonicalJson, stableSerialize,
-  CONTAINER_MAGIC, EXPORT_FORMAT_VERSION, MODE_PLAINTEXT, MODE_PASSPHRASE,
-  ExportFormatError, ExportPassphraseRequiredError, ExportDecryptionError,
+  encodeContainer,
+  decodeContainer,
+  canonicalJson,
+  stableSerialize,
+  CONTAINER_MAGIC,
+  EXPORT_FORMAT_VERSION,
+  MODE_PLAINTEXT,
+  MODE_PASSPHRASE,
+  ExportFormatError,
+  ExportPassphraseRequiredError,
+  ExportDecryptionError,
 } from "./container.js";
 export {
-  PURE_AUTHORED_TABLES, MIXED_AUTHORED_TABLES,
-  PBKDF2_ITERATIONS, PBKDF2_HASH, SALT_BYTES, NONCE_BYTES, AES_KEY_BITS,
+  PURE_AUTHORED_TABLES,
+  MIXED_AUTHORED_TABLES,
+  PBKDF2_ITERATIONS,
+  PBKDF2_HASH,
+  SALT_BYTES,
+  NONCE_BYTES,
+  AES_KEY_BITS,
 } from "./ports.js";
 export type {
-  ExportCryptoPort, TextCodec, ArchiveArtifact, AuthoredRow,
-  ExportSource, ArchiveManifestReader, ImportSink, RestoreTableResult, ArchivePresenceChecker,
+  ExportCryptoPort,
+  TextCodec,
+  ArchiveArtifact,
+  AuthoredRow,
+  ExportSource,
+  ArchiveManifestReader,
+  ImportSink,
+  RestoreTableOptions,
+  RestoreTableResult,
+  ArchivePresenceChecker,
 } from "./ports.js";
